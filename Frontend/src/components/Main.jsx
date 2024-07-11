cdf467de8377f34a15bcd2b94d60505814b4ae82
Muhammad Danish <mdanishateeq@gmail.com>
2024-07-11 06:52:43 +0500

    _

--- Frontend/src/components/Main.jsx
+++ Frontend/src/components/Main.jsx
@@ -4,7 +4,7 @@ import TodoCard from './TodoCard';
 const Main = () => {
     return (
         <main className='h-screen flex justify-center relative'>
-            <img className='absolute top-0 left-0 -z-10 w-full' src="https://img.lovepik.com/background/20211021/large/lovepik-blue-background-of-science-and-technology-image_500486349.jpg" alt="" />
+            <img className='absolute top-0 left-0 -z-10 w-full' src="https://img.freepik.com/free-photo/artistic-blurry-colorful-wallpaper-background_58702-8217.jpg" alt="" />
             <TodoCard/>
         </main>
     );
--- Frontend/src/components/TodoCard.jsx
+++ Frontend/src/components/TodoCard.jsx
@@ -104,32 +104,38 @@ const TodoCard = () => {
     };
 
     return (
-        <div className='text-white bg-blue-500 h-5/6 w-1/3 m-5 rounded-lg p-5'>
-            <h2 className='text-center text-xl font-medium'>TaskFlows - Your Personal Task Organizer</h2>
-            <label htmlFor="" className='block my-2 font-medium px-1'>Add your Todo</label>
-            <input ref={inputTodo} type="text" className='rounded-full w-4/5 text-black px-2 py-1 text-xs' spellCheck='false' />
-            <button onClick={handleSave} className='w-1/6 text-center bg-blue-700 m-1 p-0.5 rounded-full hover:bg-blue-800'>
+        <div className='taskflows-container  h-5/6 w-1/3 m-28 rounded-lg p-5'>
+            <h2 className='taskflows-title text-3xl font-bold mb-4 text-center'>TaskFlows - Your Personal Task Organizer</h2>
+            <label htmlFor="" className='taskflows-label block mb-2 font-bold mx-2'>Add your Todo</label>
+            <input ref={inputTodo} type="text" className='taskflows-input border border-gray-300 px-3 py-2 rounded-lg mb-2 focus:outline-none focus:border-blue-500 w-10/12 mx-1' spellCheck='false' />
+            <button onClick={handleSave} className='taskflows-button bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'>
                 {editingTodoId !== null ? 'Save' : 'Add'}
             </button>
-            <div className='flex gap-1 items-center my-3'>
-                <input type="checkbox" checked={showFinished} onChange={handleToggleFinished} />
-                <label className='text-xs'>Show Finished</label>
+            <div className='taskflows-checkbox mt-4'>
+                <input type="checkbox" checked={showFinished} onChange={handleToggleFinished} className='form-checkbox h-5 w-5 text-blue-500' />
+                <label className='ml-2'>Show Finished</label>
             </div>
-            <div className='w-11/12 h-0.5 bg-white'></div>
-            <label htmlFor="" className='block my-2 font-medium px-1'>Your Todos</label>
+            <div className='taskflows-divider border-b border-gray-300 my-4'></div>
+            <label htmlFor="" className='taskflows-label block font-bold mb-2'>Your Todos</label>
             {todos
                 .filter(todo => (showFinished ? true : !todo.finished))
                 .map(todo => (
-                    <div key={todo._id} className='flex items-center'>
-                        <input type="checkbox" checked={todo.finished} onChange={() => toggleTodoFinished(todo._id)} className='mr-2' />
-                        <div className={`w-3/4 ${todo.finished ? 'line-through' : ''}`}>{todo.text}</div>
-                        <div className='w-1/4 flex items-center'>
-                            <button onClick={() => handleEdit(todo._id)} className='text-center bg-blue-700 text-xs m-1 p-1 px-2 rounded-full hover:bg-blue-800'><FaEdit /></button>
-                            <button onClick={() => handleDelete(todo._id)} className='text-center bg-red-700 text-xs m-1 p-1 px-2 rounded-full hover:bg-red-800'><MdDelete /></button>
+                    <div key={todo._id} className='task flex items-center mb-2'>
+                        <input type="checkbox" checked={todo.finished} onChange={() => toggleTodoFinished(todo._id)} className='form-checkbox h-5 w-5 text-blue-500' />
+                        <div className={`task-text ml-2 ${todo.finished ? 'line-through text-gray-500' : ''}`}>{todo.text}</div>
+                        <div className='task-buttons ml-auto'>
+                            <button onClick={() => handleEdit(todo._id)} className='task-button-edit mx-3 text-blue-500 hover:text-blue-600'>
+                                <FaEdit style={{ fontSize: '1.5em' }} />
+                            </button>
+                            <button onClick={() => handleDelete(todo._id)} className='task-button-delete text-red-500 hover:text-red-600'>
+                                <MdDelete style={{ fontSize: '1.5em' }} />
+                            </button>
+
                         </div>
                     </div>
                 ))}
         </div>
+
     );
 };
 
